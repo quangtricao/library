5e1c340190cf1926d0867d260bb53b371355a7ab
Quang Tri Cao <caoquangtri0110@gmail.com>
2023-12-15 11:42:38 +0200

    create BookEditForm component in page/BookEdit

--- src/pages/BookEdit.tsx
+++ src/pages/BookEdit.tsx
@@ -1,14 +1,15 @@
-import { Box, Button, TextField, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { Box, Button } from '@mui/material';
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+
 import { useAppDispatch } from '../redux/hooks';
 import { getSingleBook, updateSingleBook } from '../services/booksService';
-import Loading from '../components/Loading';
-import DeleteIcon from '@mui/icons-material/Delete';
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
-import { BookDTO } from '../types/book';
 import { getTokenFromLocalStorage } from '../utils/localStorage';
 import { setNotification } from '../redux/slices/notificationSlice';
+import { BookDTO } from '../types/book';
+import Loading from '../components/Loading';
+import BookEditForm from '../components/BookEditForm';
 
 const EditModal = () => {
   const { isbn } = useParams();
@@ -69,180 +70,9 @@ const EditModal = () => {
           Back to the book
         </Button>
       </Link>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-        <Box>
-          <Typography
-            sx={{
-              fontWeight: 'bold',
-              borderTop: '0px',
-              borderRight: '0px',
-              borderLeft: '0px',
-              borderBottom: '4px',
-              borderStyle: 'solid',
-              borderColor: 'primary.light',
-              width: 'fit-content',
-            }}
-          >
-            Basic details
-          </Typography>
-          <Box sx={{ display: 'flex', gap: '30px' }}>
-            <TextField
-              label='ISBN'
-              variant='standard'
-              value={updateBook.isbn}
-              sx={{ width: '20%' }}
-              onChange={(event) => setUpdateBook({ ...updateBook, isbn: event.target.value })}
-            />
-            <TextField
-              label='Title'
-              variant='standard'
-              value={updateBook.title}
-              sx={{ flexGrow: 1 }}
-              onChange={(event) => setUpdateBook({ ...updateBook, title: event.target.value })}
-            />
-          </Box>
-        </Box>
-
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Box sx={{ display: 'flex', gap: '10px' }}>
-            <Typography
-              sx={{
-                fontWeight: 'bold',
-                borderTop: '0px',
-                borderRight: '0px',
-                borderLeft: '0px',
-                borderBottom: '4px',
-                borderStyle: 'solid',
-                borderColor: 'primary.light',
-                width: 'fit-content',
-                marginRight: '15px',
-              }}
-            >
-              Authors:
-            </Typography>
-            {updateBook.authors.length === 0
-              ? null
-              : updateBook.authors.map((author) => (
-                  <Button
-                    key={author._id}
-                    variant='outlined'
-                    endIcon={<DeleteIcon />}
-                    sx={{
-                      fontSize: '12px',
-                      width: 'fit-content',
-                      paddingX: '10px',
-                      borderRadius: '20px',
-                    }}
-                  >
-                    {author.name}
-                  </Button>
-                ))}
-          </Box>
-        </Box>
-
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Box sx={{ display: 'flex', gap: '10px' }}>
-            <Typography
-              sx={{
-                fontWeight: 'bold',
-                borderTop: '0px',
-                borderRight: '0px',
-                borderLeft: '0px',
-                borderBottom: '4px',
-                borderStyle: 'solid',
-                borderColor: 'primary.light',
-                width: 'fit-content',
-                marginRight: '15px',
-              }}
-            >
-              Genres:
-            </Typography>
-            {updateBook.genres.length === 0
-              ? null
-              : updateBook.genres.map((genre) => (
-                  <Button
-                    key={genre._id}
-                    variant='outlined'
-                    endIcon={<DeleteIcon />}
-                    sx={{
-                      fontSize: '12px',
-                      width: 'fit-content',
-                      paddingX: '10px',
-                      borderRadius: '20px',
-                    }}
-                  >
-                    {genre.title}
-                  </Button>
-                ))}
-          </Box>
-        </Box>
-
-        <Box>
-          <Typography
-            sx={{
-              fontWeight: 'bold',
-              borderTop: '0px',
-              borderRight: '0px',
-              borderLeft: '0px',
-              borderBottom: '4px',
-              borderStyle: 'solid',
-              borderColor: 'primary.light',
-              width: 'fit-content',
-              marginRight: '15px',
-            }}
-          >
-            Publish details:
-          </Typography>
-          <Box sx={{ display: 'flex', gap: '30px' }}>
-            <TextField
-              label='Publisher'
-              value={updateBook.publisher}
-              variant='standard'
-              sx={{ flexGrow: '1' }}
-              onChange={(event) => setUpdateBook({ ...updateBook, publisher: event.target.value })}
-            />
-            <TextField
-              label='Year Published'
-              value={updateBook.publishedYear}
-              variant='standard'
-              sx={{ width: '30%' }}
-              onChange={(event) => setUpdateBook({ ...updateBook, publishedYear: Number(event.target.value) })}
-            />
-          </Box>
-        </Box>
-
-        <Box>
-          <Typography
-            sx={{
-              fontWeight: 'bold',
-              borderTop: '0px',
-              borderRight: '0px',
-              borderLeft: '0px',
-              borderBottom: '4px',
-              borderStyle: 'solid',
-              borderColor: 'primary.light',
-              width: 'fit-content',
-              marginRight: '15px',
-            }}
-          >
-            Image links:
-          </Typography>
-          <TextField
-            label='Images'
-            variant='standard'
-            onChange={(event) => setUpdateBook({ ...updateBook, image: event.target.value })}
-          />
-          <img
-            src={updateBook.image}
-            alt={`${updateBook.title}`}
-            style={{ height: '150px', width: '150px', objectFit: 'contain' }}
-          />
-        </Box>
-        <Button size='large' variant='contained' onClick={handleConfirmUpdate}>
-          Confirm
-        </Button>
-      </Box>
+      <BookEditForm updateBook={updateBook} setUpdateBook={setUpdateBook} handleConfirmUpdate={handleConfirmUpdate} />
     </Box>
   );
 };
+
 export default EditModal;
