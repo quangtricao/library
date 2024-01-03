582a378129b72f0cd0b2cf119dcd31f69bbc622e
Quang Tri Cao <caoquangtri0110@gmail.com>
2024-01-04 01:50:02 +0200

    fix config/api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
 const PRODUCTION = 'https://cqtri-library.onrender.com/api/v1';
 const DEVELOPMENT = 'http://localhost:1337/api/v1/';
 
-export const API_URL = PRODUCTION;
+export const API_URL = process.env.NODE_ENV === 'development' ? DEVELOPMENT : PRODUCTION;
